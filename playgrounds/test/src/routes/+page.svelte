fda211088566494a1f028886a353016f3251c7b8
Rogerio Amorim <rogeriol@gmail.com>
2024-01-18 13:51:18 -0300

    startint style test

---
+++ playgrounds/test/src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	let items = []
+</script>
+
+<button on:click={()=> items = [...items, items.length]}>Add Item</button>
+
+{#each items as item, i}
+<div class="card">
+	{i}
+</div>
+{/each}
+
+<style>
+	@starting-style{
+		.card{
+			opacity: 0;
+			height: 0;
+		}
+	}	
+	.card {
+		background-color: orange;
+		max-width: max-content;
+		padding: 1rem;
+		border-radius: 1rem;
+		display: grid;
+		margin: auto;
+		transform-origin: bottom;
+		overflow: hidden;
+		transition: opacity 1s, display 1s, height 1s;
+		transition-behavior: allow-discrete;
+	}
+</style>
